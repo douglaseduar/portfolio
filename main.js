74c5c18e8b755917a02afbaeaa48ecc74642c61e
douglaseduar <douglas060401@gmail.com>
2023-06-01 20:14:43 -0300

    feat: atualizando 2023

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ function adicionar(event){
     descricao = form.descricao.value;
     document.location = 'https://api.whatsapp.com/send?phone=5542999750210&text=' + descricao;
 }
+
+function idade(nascimento, hoje) {
+    var diferencaAnos = hoje.getFullYear() - nascimento.getFullYear();
+    if ( new Date(hoje.getFullYear(), hoje.getMonth(), hoje.getDate()) < 
+         new Date(hoje.getFullYear(), nascimento.getMonth(), nascimento.getDate()) )
+        diferencaAnos--;
+    return diferencaAnos;
+}
+
+document.querySelector("span").textContent = idade(new Date(2001, 04, 06), new Date())
+
+
+const date = new Date();
+const currentYear = date.getFullYear();
+
+document.querySelector(".direito").textContent = "© "+ currentYear + " Todos os direitos reservados."    
