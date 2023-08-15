58ac9aa1e3bd17b0f4e5fe1bf934c07b24d450b5
douglaseduar <douglas060401@gmail.com>
2023-08-14 22:08:17 -0300

    feat nova versao

--- main.js
+++ main.js
@@ -15,10 +15,105 @@ function idade(nascimento, hoje) {
     return diferencaAnos;
 }
 
-document.querySelector("span").textContent = idade(new Date(2001, 04, 06), new Date())
+document.querySelector("span").textContent = idade(new Date('2001', '04', '06'), new Date())
 
 
 const date = new Date();
 const currentYear = date.getFullYear();
 
 document.querySelector(".direito").textContent = "© "+ currentYear + " Todos os direitos reservados."    
+
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo3() {
+    for (let i = 100; i <= 600; i++) {
+        document.querySelector(".artes").textContent = i
+        await sleep(2);
+    }
+
+}
+async function demo1() {
+    for (let i = 0; i <= 22; i++) {
+        document.querySelector(".proje").textContent = i
+        await sleep(100);
+    }
+}
+async function demo2() {
+    for (let i = 0; i <= 5; i++) {
+        document.querySelector(".sitess").textContent = i
+        await sleep(200);
+    }
+}
+
+function demo(){
+    if(document.querySelector(".proje").textContent == '0'){
+        demo3();
+        demo1();
+        demo2();
+    }
+
+}
+
+function frase(msg){
+    document.querySelector(".texto1").textContent = msg
+}
+
+function frasen(){
+    document.querySelector(".texto1").innerHTML = "&nbsp"
+}
+
+
+document.querySelector('#profissional').addEventListener("click", alterar);
+
+function alterar(){
+    document.querySelector(".graducao").style.display = 'none' 
+    document.querySelector(".curso").style.display = 'none' 
+    document.querySelector(".profissional").style.display = 'block' 
+    document.querySelector("#profissional").style.textDecoration = 'overline';
+    document.querySelector("#graducao").style.textDecoration = 'none';
+    document.querySelector("#curso").style.textDecoration = 'none';
+
+}
+document.querySelector('#graducao').addEventListener("click", alterar1);
+
+function alterar1(){
+    document.querySelector(".graducao").style.display = 'block'
+    document.querySelector(".profissional").style.display = 'none' 
+    document.querySelector(".curso").style.display = 'none' 
+    document.querySelector("#graducao").style.textDecoration = 'overline';
+    document.querySelector("#curso").style.textDecoration = 'none';
+    document.querySelector("#profissional").style.textDecoration = 'none';
+
+}
+document.querySelector('#curso').addEventListener("click", alterar2);
+
+function alterar2(){
+    document.querySelector(".graducao").style.display = 'none'
+    document.querySelector(".profissional").style.display = 'none' 
+    document.querySelector(".curso").style.display = 'block' 
+    document.querySelector("#curso").style.textDecoration = 'overline';
+    document.querySelector("#profissional").style.textDecoration = 'none';
+    document.querySelector("#graducao").style.textDecoration = 'none';
+}
+
+
+rodar();
+
+async function rodar(){
+    for(let i = 0; i < 1500; i++)
+    {
+        await sleep(10);
+        if($('#sites').is(':hover')){
+            break
+        }
+
+         document.querySelector(".sites").scrollTo(i, 0)
+    }
+    await sleep(100);
+    rodar()
+    
+}
